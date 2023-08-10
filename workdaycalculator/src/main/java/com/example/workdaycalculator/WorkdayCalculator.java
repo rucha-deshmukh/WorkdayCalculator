51edbe6feae174214c540041b04c25926e53679d
Rucha <kruc.nd1@gmail.com>
2023-08-10 22:35:00 +0530

    add entity

---
+++ workdaycalculator/src/main/java/com/example/workdaycalculator/WorkdayCalculator.java
@@ -0,0 +1,13 @@
+package com.example.workdaycalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkdayCalculator {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkdayCalculator.class, args);
+	}
+
+}
